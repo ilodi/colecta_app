2991fdcbff58132caa6ad99f5f8d31d94f2fde97
lodi_windows <lodi.dev@outlook.com>
2020-01-22 00:25:39 -0600

    add for mul_providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,28 @@
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 //Screens
 import './src/pages/home_page.dart';
 import 'src/pages/login_page.dart';
 import 'package:colecta/src/providers/heroesinfo.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-     
-      create:(context) => HeroesInfo(),
-
-          child: MaterialApp(
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => HeroesInfo(),
+        ),
+      ],
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Colecta App',
         initialRoute: 'home',
         routes: {
-          'login' : (BuildContext context) => LoginPage(),
-          'home':(BuildContext context) => HomePage(),
-          
+          'login': (BuildContext context) => LoginPage(),
+          'home': (BuildContext context) => HomePage(),
         },
       ),
     );
